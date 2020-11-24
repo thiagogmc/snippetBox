f45e5a10725f7896e92e04580e14157fa5a8171f
Thiago Costa <thgmcosta@gmail.com>
2020-11-24 11:09:05 -0300

    3.5 Isolating the Application Routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -24,20 +24,12 @@ func main() {
 		errorLog: errorLog,
 		infoLog:  infoLog,
 	}
-	// Use the http.NewServeMux() function to initialize a new servemux, then
-	//register the home function as the handler for the "/" URL pattern.
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
---
+++ cmd/web/routes.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	// Use the http.NewServeMux() function to initialize a new servemux, then
+	//register the home function as the handler for the "/" URL pattern.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	return mux
+}
