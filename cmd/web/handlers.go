4d1204fb7e380a50c34d211ad3c549dbfa4cbc4b
Thiago Costa <thgmcosta@gmail.com>
2020-12-01 13:05:31 -0300

    5.2 Template Actions and Functions

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -26,28 +26,24 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, snippet := range s {
-		fmt.Fprintf(w, "%v\n", snippet)
+	data := &templateData{Snippets: s}
+
+	files := []string {
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
 	}
 
-	//files := []string {
-	//	"./ui/html/home.page.tmpl",
-	//	"./ui/html/base.layout.tmpl",
-	//	"./ui/html/footer.partial.tmpl",
-	//}
-	//
-	//ts, err := template.ParseFiles(files...)
-	//if err != nil {
-	//	app.serverError(w, err)
-	//	return
-	//}
-	//
-	//err = ts.Execute(w, s)
-	//if err != nil {
-	//	app.serverError(w, err)
-	//}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
 
-	w.Write([]byte("Hello from Snippetbox"))
+	err = ts.Execute(w, data)
+	if err != nil {
+		app.serverError(w, err)
+	}
 }
 
 // Add a showSnippet handler function.
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -4,4 +4,5 @@ import "github.com/thiagogmc/snippetbox/pkg/models"
 
 type templateData struct {
 	Snippet *models.Snippet
+	Snippets []*models.Snippet
 }
