e77c6dca1a19d5c92a3d037347e524973e6b09df
Thiago Costa <thgmcosta@gmail.com>
2020-12-03 14:50:32 -0300

    5.6 Custom template functions

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -4,12 +4,17 @@ import (
 	"github.com/thiagogmc/snippetbox/pkg/models"
 	"html/template"
 	"path/filepath"
+	"time"
 )
 
 type templateData struct {
 	CurrentYear int
 	Snippet *models.Snippet
 	Snippets []*models.Snippet
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
@@ -22,7 +27,7 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
@@ -40,4 +45,8 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 		cache[name] = ts
 	}
 	return cache, nil
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
 }
