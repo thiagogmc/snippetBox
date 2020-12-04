724d79d5ae1f6b4edc475fd9845767e618d21714
Thiago Costa <thgmcosta@gmail.com>
2020-12-04 11:53:31 -0300

    6.4 Panic Recovery

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
@@ -16,5 +19,17 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 		app.infoLog.Printf("%s - %s %s %s", request.RemoteAddr, request.Proto, request.Method, request.URL)
 
 		next.ServeHTTP(writer, request)
+	})
+}
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("Connection", "close")
+				app.serverError(w, fmt.Errorf("%s", err))
+			}
+		}()
+		next.ServeHTTP(w, r)
 	})
 }
