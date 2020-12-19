37c380f5f8ed06dc653788696de397f7c43a08e3
Thiago Costa <thgmcosta@gmail.com>
2020-12-19 16:10:32 -0300

    13.3 End-To-End Testing

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,32 +1,48 @@
 package main
 
 import (
-	"io/ioutil"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
+//func TestPing(t *testing.T) {
+//	rr := httptest.NewRecorder()
+//
+//	r, err := http.NewRequest("GET", "/", nil)
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	ping(rr, r)
+//
+//	rs := rr.Result()
+//
+//	if rs.StatusCode !=  http.StatusOK {
+//		t.Errorf("want %d; got %d", http.StatusOK, rs.StatusCode)
+//	}
+//
+//	defer rs.Body.Close()
+//	body, err := ioutil.ReadAll(rs.Body)
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	if string(body) != "OK" {
+//		t.Errorf("want body to equal %q", "OK")
+//	}
+//}
+//
+
 func TestPing(t *testing.T) {
-	rr := httptest.NewRecorder()
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
 
-	r, err := http.NewRequest("GET", "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	code, _, body := ts.get(t, "/ping")
 
-	ping(rr, r)
 
-	rs := rr.Result()
-
-	if rs.StatusCode !=  http.StatusOK {
-		t.Errorf("want %d; got %d", http.StatusOK, rs.StatusCode)
-	}
-
-	defer rs.Body.Close()
-	body, err := ioutil.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
+	if code != http.StatusOK {
+		t.Errorf("want %d; got %d", http.StatusOK, code)
 	}
 
 	if string(body) != "OK" {
